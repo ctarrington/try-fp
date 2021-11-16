1d30d7062fc8da1bab0df614db8b044068a3254f
ctarrington <arrington.ct@gmail.com>
2021-11-15 21:17:09 -0500

    basics of FP list

--- linked-lists/README.md
+++ linked-lists/README.md
@@ -1,8 +1,7 @@
-## baseline in rust with old school imperative
-
 Based on https://rust-unofficial.github.io/too-many-lists/index.html
 
 To run:    
+cd to a cargo containing project
 find . -name "*.rs" | entr -r cargo test
 
 
---
+++ linked-lists/fp-rust/persistent-list/src/third.rs
@@ -0,0 +1,64 @@
+// following along with
+// https://rust-unofficial.github.io/too-many-lists/third.html
+
+use std::rc::Rc;
+// ------------------- Core data structure and List API ----------------------------------
+
+// something small to expose publicly
+pub struct PersistentList<T> {
+    head: Link<T>,
+}
+
+type Link<T> = Option<Rc<Node<T>>>;
+
+struct Node<T> {
+    next: Link<T>,
+    element: T,
+}
+
+impl<T> PersistentList<T> {
+    pub fn new() -> Self {
+        PersistentList { head: None }
+    }
+
+    // returns a List with the new value prepended to the front of the old List
+    // the old List still exists
+    pub fn prepend(&self, value: T) -> PersistentList<T> {
+        PersistentList {
+            head: Some(Rc::new(Node {
+                element: value,
+                next: self.head.clone(),
+            })),
+        }
+    }
+
+    pub fn tail(&self) -> PersistentList<T> {
+        PersistentList {
+            head: self.head.as_ref().and_then(|node| node.next.clone()),
+        }
+    }
+
+    pub fn head(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| &node.element)
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::PersistentList;
+
+    #[test]
+    fn simple() {
+        let empty_list = PersistentList::new();
+        assert_eq!(empty_list.head(), None);
+
+        let list_1 = empty_list.prepend(1);
+        assert_eq!(empty_list.head(), None);
+        assert_eq!(list_1.head(), Some(&1));
+
+        let list_321 = list_1.prepend(2).prepend(3);
+        let list_21 = list_321.tail();
+        assert_eq!(list_321.head(), Some(&3));
+        assert_eq!(list_21.head(), Some(&2));
+    }
+}

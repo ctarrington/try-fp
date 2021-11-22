63559ec6abd451c8a1decad5981a20fee498a171
CT Arrington <arrington.ct@gmail.com>
2021-11-22 11:01:36 -0500

    added docs as an excercise

--- linked-lists/fp-rust/persistent-list/src/third.rs
+++ linked-lists/fp-rust/persistent-list/src/third.rs
@@ -1,9 +1,13 @@
-// following along with
-// https://rust-unofficial.github.io/too-many-lists/third.html
+//! A simple implementation of a persistent list   
+//! following along with <https://rust-unofficial.github.io/too-many-lists/third.html>
+
+#![warn(missing_docs)]
 
 use std::rc::Rc;
 
 // ------------------- Core data structure and List API ----------------------------------
+
+/// A PersistentList cannot be changed - adding or removing returns a new List
 
 // something small to expose publicly
 pub struct PersistentList<T> {
@@ -18,6 +22,7 @@ struct Node<T> {
 }
 
 impl<T> PersistentList<T> {
+    /// Create an empty PersistentList
     pub fn new() -> Self {
         Self { head: None }
     }
@@ -30,17 +35,35 @@ impl<T> Default for PersistentList<T> {
 }
 
 impl<T> PersistentList<T> {
-    // returns a List with the new value prepended to the front of the old List
-    // the old List still exists
+    /// Returns a List with the new value prepended to the front of the old List
+    /// The original List still exists
+    /// ```
+    /// use persistent_list::third::PersistentList;
+    ///    let empty_list = PersistentList::new();
+    ///    assert_eq!(empty_list.head(), None);
+    ///    assert_eq!(empty_list.tail().head(), None);
+    ///
+    ///    let list_1 = empty_list.prepend(1);
+    ///    assert_eq!(empty_list.head(), None);
+    ///    assert_eq!(list_1.head(), Some(&1));
+    ///
+    ///    let list_321 = list_1.prepend(2).prepend(3);
+    ///    let list_21 = list_321.tail();
+    ///    assert_eq!(list_321.head(), Some(&3));
+    ///    assert_eq!(list_21.head(), Some(&2));
+    /// ```
     pub fn prepend(&self, value: T) -> Self {
         Self {
             head: Some(Rc::new(Node {
                 element: value,
-                next: self.head.as_ref().map(|rc_node| Rc::clone(&rc_node)), // use Rc::clone to make it explicit that we are only bumping the reference count
+                // use Rc::clone to make it explicit that we are only bumping the reference count
+                next: self.head.as_ref().map(|rc_node| Rc::clone(&rc_node)),
             })),
         }
     }
 
+    /// Returns a List with the first item removed
+    /// The original List still exists
     pub fn tail(&self) -> Self {
         Self {
             head: self
@@ -50,6 +73,7 @@ impl<T> PersistentList<T> {
         }
     }
 
+    /// Returns the first value held by the list
     pub fn head(&self) -> Option<&T> {
         self.head.as_ref().map(|node| &node.element)
     }
@@ -57,11 +81,14 @@ impl<T> PersistentList<T> {
 
 // ------------------- iteration over references to Ts ----------------------------------
 
+/// struct that is returned from iter. No need for client code to create these or worry about them
+/// in any way.
 pub struct Iter<'a, T> {
     next: Option<&'a Node<T>>,
 }
 
 impl<T> PersistentList<T> {
+    /// Returns an iterator over the items in the list
     pub fn iter(&self) -> Iter<'_, T> {
         Iter {
             next: self.head.as_deref(),
